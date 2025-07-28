a6c986dcce433d84762c047ad948b586f9b968c1
HilaWix <hilat@wix.com>
2025-07-28 18:55:39 +0300

    Update monday.js

--- monday.js
+++ monday.js
@@ -1,53 +1,50 @@
-const axios = require("axios");
+const axios = require('axios');
+const FormData = require('form-data');
 
-const MONDAY_API_URL = "https://api.monday.com/v2";
-const MONDAY_API_TOKEN = process.env.MONDAY_API_TOKEN;
-const MONDAY_BOARD_ID = process.env.MONDAY_BOARD_ID;
+const MONDAY_API_URL    = 'https://api.monday.com/v2';
+const MONDAY_API_TOKEN  = process.env.MONDAY_API_TOKEN;
+const MONDAY_BOARD_ID   = process.env.MONDAY_BOARD_ID;
 
 const columnMapping = {
-  articleLink: "link",           // Link to Article
-  requestType: "status_1",       // Task Type
-  description: "long_text",      // Description
-  attachments: "files",          // File(s) [optional]
-  requestor: "person"            // Creator
+  articleLink:  'link',       // Link to Article
+  requestType:  'status_1',   // Task Type
+  description:  'long_text',  // Description
+  attachments:  'files',      // File(s) [optional]
+  requestor:    'person'      // Creator
 };
 
-const escapeQuotes = str => (str ? str.replace(/"/g, '\\"') : "");
+const escapeQuotes = str => (str ? str.replace(/"/g, '\\"') : '');
 
-const createMondayItem = async ({ articleLink, requestType, description, user }) => {
-  const query = `
-    mutation {
+const createMondayItem = async ({ articleLink, requestType, description, user, files = [] }) => {
+  // 1) Build the column_values object
+  const columnValues = {
+    [columnMapping.articleLink]:  { url: articleLink, text: 'KB Article' },
+    [columnMapping.requestType]:  { label: requestType },
+    [columnMapping.description]:  description
+  };
+
+  // 2) Create the item
+  const createQuery = `
+    mutation ($boardId: Int!, $columnVals: JSON!) {
       create_item(
-        board_id: ${MONDAY_BOARD_ID},
+        board_id: $boardId,
         item_name: "KB Request from ${escapeQuotes(user)}",
-        column_values: "{
-          \\"${columnMapping.articleLink}\\": {\\"url\\": \\"${escapeQuotes(articleLink)}\\", \\"text\\": \\"KB Article\\"},
-          \\"${columnMapping.requestType}\\": {\\"label\\": \\"${escapeQuotes(requestType)}\\"},
-          \\"${columnMapping.description}\\": \\"${escapeQuotes(description)}\\"
-        }"
-      ) {
-        id
-      }
+        column_values: $columnVals
+      ) { id }
     }
   `;
-
-  try {
-    const response = await axios.post(MONDAY_API_URL, { query }, {
-      headers: {
-        Authorization: MONDAY_API_TOKEN,
-        "Content-Type": "application/json"
+  const createRes = await axios.post(MONDAY_API_URL,
+    { query: createQuery,
+      variables: {
+        boardId: MONDAY_BOARD_ID,
+        columnVals: columnValues
       }
-    });
+    },
+    { headers: { Authorization: MONDAY_API_TOKEN } }
+  );
+  const itemId = createRes.data.data.create_item.id;
 
-    if (response.data.errors) {
-      console.error("Monday.com API error:", response.data.errors);
-      throw new Error(response.data.errors[0].message);
-    }
-    return response.data.data.create_item.id;
-  } catch (error) {
-    console.error("Error creating Monday.com item:", error.message);
-    throw error;
-  }
-};
-
-module.exports = { createMondayItem };
+  // 3) Attach each Slack file
+  for (const slackFileId of files) {
+    // a) get download URL
+    const info = await axios.post('https://slack.com/api/f
